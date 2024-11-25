18ce00aa51579e51a7c31f99d2123f0656d4c526
nhutlin <22012003linh@gmail.com>
2024-11-25 23:00:54 +0700

    fix cors

--- Cart/server.js
+++ Cart/server.js
@@ -7,17 +7,16 @@ require('dotenv').config();
 require('./config/db_conn');
 const port = process.env.PORT || 9003;
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(
+    cors({
+      origin: '*',
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      credentials: true,
+    })
+  );
 
 
 app.use("/cart", require("./routes/cartRouter"))
--- Product/server.js
+++ Product/server.js
@@ -7,7 +7,13 @@ require('dotenv').config();
 require('./config/db_conn');
 const port = process.env.PORT || 9000;
 
-app.use(cors());
+app.use(
+    cors({
+      origin: '*',
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      credentials: true,
+    })
+  );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
